c19bf408693a9bd8b49d10a205b055e3ae8c6dad
artem <artem.gaponov.2024@mail.ru>
2026-01-12 00:24:35 +0300

    Добавлено решение

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -33,7 +33,19 @@ class DeliveryTest {
         $("[data-test-id='date'] input").setValue(firstMeetingDate);
         $("[data-test-id='name'] input").setValue(validUser.getName());
         $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $(Selectors.byText("Запланировать")).click();
 
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
+                .shouldBe(visible);
 
+        $("[data-test-id='date'] input").press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(secondMeetingDate);
+        $(Selectors.byText("Запланировать")).click();
+
+        // $("[data-test-id='success-notification'] .notification__content")
+        //                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
+        //                .shouldBe(visible);
     }
 }
